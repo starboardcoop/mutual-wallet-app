064c22a7398a298d11c6641156034ae686161859
Dillon Fagan <dillonfagan@pm.me>
2022-04-06 09:30:56 -0400

    Use composition to make ExchangeButton

--- lib/widgets/exchange_button.dart
+++ lib/widgets/exchange_button.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ExchangeButton extends FloatingActionButton {
-  const ExchangeButton({required VoidCallback? onPressed})
-      : super(
-          onPressed: onPressed,
-          child: const Icon(
-            Icons.swap_horiz,
-            size: 32,
-          ),
-        );
+class ExchangeButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+
+  const ExchangeButton({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      child: const Icon(Icons.swap_horiz, size: 32),
+    );
+  }
 }
